aeb5bab99dd89b3d59d3b908dad75e8423ea320c
edlo <b93303015@gmail.com>
2020-10-30 00:37:10 +0800

    Adds mapStore to replace normal store oprations; Change table and playground style; \nAdds clear button to cancel selected cards

--- src/store/index.js
+++ src/store/index.js
@@ -10,14 +10,19 @@ export default createStore({
   mutations: {
       add(state, payload) {
           state.cardList[payload] = [];
-          // console.log(state.cardList[payload], payload);
       },
       push(state, payload) {
           state.cardList[payload.index].push(...payload.list);
       },
       delete(state, payload) {
           state.cardList[0] = state.cardList[0].filter(card => payload.indexOf(card.index) < 0);
-          // console.log('aft', state.cardList[0], payload);
+      },
+      selected(state, payload) {
+        let card = state.cardList[0].filter(card => card.index === payload)[0];
+        card.isSelected = !card.isSelected;
+      },
+      clear(state) {
+        state.cardList[0].forEach(card => card.isSelected = false);
       },
       replace(state, payload) {
         state.selectedList = payload;
